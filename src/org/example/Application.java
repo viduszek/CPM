8c270fd496b5fcdaf474a355177edc388875ec79
PawelZieba2000 <pawel.zieba2000@gmail.com>
2023-03-27 14:36:16 +0200

    prace w toku

--- src/org/example/Application.java
+++ src/org/example/Application.java
@@ -10,7 +10,8 @@ public class Application implements ActionListener {
     private JFrame frame;
     private JPanel panel;
     private JLabel welcome_text;
-    private JButton button_1, button_2;
+    private JButton button_CPM, button_posrednik, button_add;
+    private JTextField name, time, prevs;
     private List<Node> nodes;
     //implement main function here
     public static void main(String[] args) {
@@ -25,28 +26,46 @@ public class Application implements ActionListener {
         frame.add(panel);
         panel.setLayout(null);
 
-        welcome_text = new JLabel("Witaj\nwybierz metodę wprowadzania danych:");
-        button_1 = new JButton("poprzednicy");
-        button_2 = new JButton("kolejność zdarzeń");
+        welcome_text = new JLabel("Witaj! Wybierz metodę:");
+        button_CPM = new JButton("CPM");
+        button_posrednik = new JButton("Pośrednik");
+        button_add = new JButton("Add");
+        button_add.addActionListener(this);
 
         welcome_text.setBounds(10,10,400,25);
-        button_1.setBounds(10,60,180,25);
-        button_2.setBounds(210,60,180,25);
-        button_1.addActionListener(this);
-        button_2.addActionListener(this);
+        button_CPM.setBounds(10,60,180,25);
+        button_posrednik.setBounds(210,60,180,25);
+        button_CPM.addActionListener(this);
+        button_posrednik.addActionListener(this);
         panel.add(welcome_text);
-        panel.add(button_1);
-        panel.add(button_2);
+        panel.add(button_CPM);
+        panel.add(button_posrednik);
 
         frame.setVisible(true);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==button_1){
-            System.out.println("poprzednicy");
+        if(e.getSource()== button_CPM){
+            System.out.println("CPM");
+            panel.removeAll();
+            name = new JTextField("");
+            time = new JTextField("");
+            prevs = new JTextField("");
+            name.setBounds(10,10,50,25);
+            time.setBounds(70,10,50,25);
+            prevs.setBounds(130,10,50,25);
+            button_add.setBounds(190,10,50,25);
+
+            panel.add(name);
+            panel.add(time);
+            panel.add(prevs);
+            panel.add(button_add);
+
+            if(e.getSource()==button_add) System.out.println("add");
+
         }
-        else if (e.getSource()==button_2) {
-            System.out.println("kolejnosc zdarzen");
+        else if (e.getSource()== button_posrednik) {
+            System.out.println("posrednicy");
         }
     }
 }
